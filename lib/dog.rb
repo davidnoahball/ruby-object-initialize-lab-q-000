7971bc682f0ace8475ad4281b7560ed7f3f3cc0d
davidnoahball <davidnoahball@gmail.com>
2016-04-30 18:28:51 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+  def initialize(name, breed = "Mut")
+    @name = name
+    @breed = breed
+  end
+  def name
+    @name
+  end
+  def name=(input)
+    @name = input
+  end
+  def breed
+    @breed
+  end
+  def breed=(input)
+    @breed = input
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person
+  def initialize(name = "Dog")
+    @name = name
+  end
+  def name
+    @name
+  end
+  def name=(input)
+    @name = input
+  end
+end
